7976c4d837d1f66335c120e136f35e5108f9179a
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2025-08-10 03:07:26 -0300

    .

--- src/components/inicio/perfilComponent.vue
+++ src/components/inicio/perfilComponent.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
 import useUserStore from 'src/stores/userStore';
+import { useRouter } from 'vue-router';
 
 const userStore = useUserStore();
+const router = useRouter();
+
+const logar = () => {
+  if (!userStore.logado) {
+    router.push('/login').catch((error) => {
+      console.error('Erro ao navegar:', error);
+    });
+  }
+};
 </script>
 
 <template>
-  <div id="perfil">
-    <q-avatar size="70px">
-      <img :src="userStore.perfil" alt="">
+  <div id="perfil" @click="logar" class=".nLogado">
+    <q-avatar size="70px" v-if="userStore.logado">
+      <img :src="userStore.perfil" alt="" />
     </q-avatar>
-    <span id="nomeUser">{{ userStore.nome }}</span>
+    <perfil-icon v-else />
+    <span id="nomeUser" v-if="userStore.logado">{{ userStore.nome }}</span>
+    <span id="nomeUser" v-else>Entrar</span>
   </div>
 </template>
 
 <style scoped>
-  #nomeUser{
-    margin-left: 10px;
-    font-size: 16px;
-  }
+#nomeUser {
+  margin-left: 10px;
+  font-size: 16px;
+}
+
+.nLogado{
+  cursor: pointer;
+}
 </style>
