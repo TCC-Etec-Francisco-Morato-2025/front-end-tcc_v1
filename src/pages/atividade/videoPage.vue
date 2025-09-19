2de9620f18bbf72494921eb4fc3865d566a9305b
cleitinho69 <ezequielsantos2018@outlook.com.br>
2025-09-19 17:00:49 +0200

    .

--- src/pages/atividade/videoPage.vue
+++ src/pages/atividade/videoPage.vue
@@ -3,12 +3,12 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 import usePopUpStore from 'src/stores/popUp';
 import pauseComponent from 'src/components/atividade/pauseComponent.vue';
 import screenRotateComponent from 'src/components/atividade/video/screenRotateComponent.vue';
-import useMateriaStore from 'src/stores/materiaStore';
+// import useMateriaStore from 'src/stores/materiaStore';
 import videojs from 'video.js'
 
 const popUpStore = usePopUpStore();
-const materiaStore = useMateriaStore();
-const corFundo = ref(materiaStore.cor);
+// const materiaStore = useMateriaStore();
+// const corFundo = ref(materiaStore.cor);
 
 const videoPlayer = ref<HTMLVideoElement | null>(null)
 let player: ReturnType<typeof videojs> | null = null
@@ -23,6 +23,9 @@ onMounted(() => {
       preload: 'auto',
       fluid: true, // responsivo
     })
+    if (pergunta.value) {
+      player.pause();
+    }
   }
   popUpStore.togglePerguntas();
 })
@@ -30,6 +33,16 @@ onMounted(() => {
 onBeforeUnmount(() => {
   if (player) player.dispose()
 })
+
+
+// controle das perguntas
+const pergunta = ref(true);
+const value = ref(30);
+
+setInterval(() => {
+  value.value--
+}, 1000)
+
 </script>
 
 <template>
@@ -38,21 +51,75 @@ onBeforeUnmount(() => {
     <screen-rotate-component />
     <main class="center">
       <div class="box-video">
-        <video ref="videoPlayer" class="video-js vjs-big-play-centered">
+        <video ref="videoPlayer" class="video-js vjs-big-play-centered" loop>
           <source src="/src/assets/aulas/COSTA RICA IN 4K 60fps HDR (ULTRA HD).mp4" type="video/mp4" />
         </video>
         <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/LXb3EKWsInQ?si=M607DeOFOj1o8XT5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
       </div>
-      <q-dialog v-model="popUpStore.perguntas" persistent>
-        <q-card class="perguntas">
-          test
-        </q-card>
-      </q-dialog>
+      <q-card class="questoes">
+        <q-card-section class="pergunta">
+          <q-avatar>
+            <q-knob readonly show-value font-size="12px" v-model="value" size="50px" :max="30" :thickness="0.22"
+              color="teal" track-color="grey-3" class="q-ma-md">
+              {{ value }}
+            </q-knob>
+          </q-avatar>
+          <p>Qual a equação mais se adequal para essa situação?</p>
+        </q-card-section>
+        <q-card-actions class="resposta">
+          <q-list>
+            <q-item dense clickable v-ripple>
+              <q-item-section avatar class="center">
+                <div class="numero-resposta center">
+                  A
+                </div>
+              </q-item-section>
+
+              <q-item-section class="label-resposta">Lorem ipsum dolor sit amet consectetur adipisicing elit.</q-item-section>
+            </q-item>
+            <q-item dense clickable v-ripple>
+              <q-item-section avatar class="center">
+                <div class="numero-resposta center">
+                  B
+                </div>
+              </q-item-section>
+
+              <q-item-section class="label-resposta">Lorem ipsum</q-item-section>
+            </q-item>
+            <q-item dense clickable v-ripple>
+              <q-item-section avatar class="center">
+                <div class="numero-resposta center">
+                  C
+                </div>
+              </q-item-section>
+
+              <q-item-section class="label-resposta">Lorem ipsum dolor sit</q-item-section>
+            </q-item>
+            <q-item dense clickable v-ripple>
+              <q-item-section avatar class="center">
+                <div class="numero-resposta center">
+                  D
+                </div>
+              </q-item-section>
+
+              <q-item-section class="label-resposta">Lorem ipsum dolor sit amet consectetur adipisicing elit. fdnjism smjd msdkolasn fjdjsn</q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-actions>
+        <q-card-section>
+
+        </q-card-section>
+      </q-card>
     </main>
   </q-page>
 </template>
 
 <style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
 main {
   width: 100dvw;
   height: 100dvh;
@@ -61,7 +128,7 @@ main {
   /* background-color: v-bind(corFundo); */
 }
 
-.q-card.perguntas{
+.q-card.perguntas {
   width: 300px;
   height: auto;
 }
@@ -73,7 +140,53 @@ main {
 }
 
 .video-js {
-  width: 80dvw;
+  /* width: 80dvw; */
+  width: 50dvw;
   height: auto;
+  scale: 0.8;
+}
+
+/* config questões */
+.questoes {
+  height: 350px;
+  width: 400px;
+  margin-right: 20px;
+}
+
+.pergunta {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 20px 0 20px;
+  font-size: 120%;
+  gap: 10px;
+}
+
+.resposta {
+  flex-direction: column;
+}
+
+.resposta .q-list {
+  width: 80%;
+}
+
+.q-item{
+  min-height: 40px;
+  margin: 20px 0;
+  width: 100%;
+  border-radius: 20px;
+  background-color: var(--cor-principal-1) !important;
+}
+
+.numero-resposta {
+  width: 30px;
+  height: 30px;
+  border: 2px solid white;
+  border-radius: 100%;
+}
+
+.label-resposta{
+  font-size: 80%;
+  padding-right: 5px;
 }
 </style>
