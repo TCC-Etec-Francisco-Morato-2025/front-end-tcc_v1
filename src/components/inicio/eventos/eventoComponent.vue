780476377f3e9cbfe99cf50fea77b47f0f62c218
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2025-10-02 11:29:06 -0300

    .

--- src/components/inicio/eventos/eventoComponent.vue
+++ src/components/inicio/eventos/eventoComponent.vue
@@ -1,7 +1,22 @@
 <template>
-  <q-card>
-    <q-card-section>
-      <q-img src="/src/assets/eventos/novosEquipamentos.jpeg" :ratio="16/9"/>
-    </q-card-section>
+  <q-card align="center" flat>
+    <div class="filtro"></div>
+      <q-img src="/src/assets/eventos/novosEquipamentos.jpeg"/>
   </q-card>
 </template>
+
+<style scoped>
+
+.q-img{
+  width: 100%;
+  height: 100%;
+}
+
+.filtro{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.384);
+  z-index: 1;
+}
+</style>
