963b63f8ec088d17bc67fd5cd7c7e1f8c5546da6
Etec <etec@etec.com>
2025-10-21 10:39:41 -0300

    .

--- src/stores/loginStore.ts
+++ src/stores/loginStore.ts
@@ -2,7 +2,12 @@ import { defineStore } from 'pinia';
 import useUserStore from './userStore';
 import { api } from 'src/boot/axios';
 
-const userStore = useUserStore();
+interface loginVariable {
+  file?: null | File;
+  nome?: string;
+  senha: string;
+  email: string;
+}
 
 const useLoginStore = defineStore('login', {
   state: () => ({
@@ -16,39 +21,96 @@ const useLoginStore = defineStore('login', {
     loading: false,
   }),
   actions: {
-    async login() {
-      this.loading = true;
+    async login(senha: string, email: string) {
+      const formData = new FormData();
 
-      this.query = `
-      query loginUser($email:String!, $password:String!){
-        user(email: $email, password: $password){
-          username
-          email
+      const variables: loginVariable = {
+        senha,
+        email,
+      };
+
+      const operations = {
+        query: `
+      mutation Register($email: String!, $senha: String!) {
+        login(email:$email,password:$senha){
+          token
+          user{
+            id
+            username
+            foto
+          }
         }
-      }`;
-
-      if (userStore.email && userStore.password) {
-        this.variables.email = userStore.email;
-        this.variables.password = userStore.password;
-      } else{
-        return;
       }
+    `,
+        variables,
+      };
 
-      const response = await api.post('', { query: this.query, variables: this.variables });
+      formData.append('operations', JSON.stringify(operations));
 
       try {
-        if (response.data && response.data.data) {
-          userStore.mudarUser(response.data.data);
-        } else {
-          console.error('Dados da resposta não encontrados ou formatados de maneira incorreta.');
-        }
+        this.loading = true;
+
+        const response = await api.post('', formData, {
+          withCredentials: true,
+        });
+
+        console.log(response.data.data);
       } catch (error) {
-        console.log(error);
+        console.error(error);
+        throw error;
       } finally {
         this.loading = false;
       }
     },
-    async register() {},
+    async register(nome: string, email: string, senha: string, selectedFile: File | null) {
+      const formData = new FormData();
+
+      const variables: loginVariable = {
+        file: null,
+        nome,
+        senha,
+        email,
+      };
+
+      const operations = {
+        query: `
+      mutation Register($file: Upload, $email: String!, $senha: String!, $nome: String!) {
+        register(uuid: null, email: $email, password: $senha, username: $nome, foto: $file) {
+          token
+          user {
+            id
+            username
+            foto
+          }
+        }
+      }
+    `,
+        variables,
+      };
+
+      formData.append('operations', JSON.stringify(operations));
+
+      // 3. Se tiver arquivo, inclui o mapeamento e o arquivo
+      if (selectedFile) {
+        formData.append('map', JSON.stringify({ '0': ['variables.file'] }));
+        formData.append('0', selectedFile);
+      }
+
+      try {
+        this.loading = true;
+
+        const response = await api.post('', formData, {
+          withCredentials: true,
+        });
+
+        console.log(response.data.data);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      } finally {
+        this.loading = false;
+      }
+    },
 
     async manterLogin() {
       this.loading = true;
